4df37d12047701dd2f716d67ae4c552546b33d7c
Pranshu Patel <pranshu.patel@simformsolutions.com>
2025-06-06 14:17:01 +0530

    Add: config react main.tsx file

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+function App() {
+    return (
+        <h1>APP Component</h1>
+    );
+}
+
+export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ module.exports={
     module: {
         rules: [
             {
-                test: /\.(scss|sass|css)$/,
+                test: /\.(sa|sc|c)ss$/,
                 use: [
                     'style-loader',
                     'css-loader',
