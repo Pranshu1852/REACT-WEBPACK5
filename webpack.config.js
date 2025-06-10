2359ddc01f28cfef9562d6797f4ab93b9337a24f
Pranshu Patel <pranshu.patel@simformsolutions.com>
2025-06-10 10:35:14 +0530

    Add: config mode and devtool based on the NODE_ENV environment variable

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,10 @@
 const path=require('path');
 const HtmlWebpackPlugin=require('html-webpack-plugin');
 
+const isDevelopment = process.env.NODE_ENV !== 'production';
+
 module.exports={
-    mode: "development",
+    mode: isDevelopment ? 'development' : 'production',
     entry: {
         bundle: path.resolve(__dirname, 'src/main.tsx') 
     },
@@ -15,7 +17,7 @@ module.exports={
         clean: true,
         assetModuleFilename: '[name][ext]'
     },
-    devtool: 'source-map',
+    devtool: isDevelopment ? 'eval-source-map' : 'source-map',
     devServer: {
         static: {
             directory: path.resolve(__dirname, 'dist')
